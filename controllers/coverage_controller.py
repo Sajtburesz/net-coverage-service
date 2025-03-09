3184840bbdd13ef3058bb657c933f16c9b81056d
bencebihacsi <bence.bihacsi@smartpoint.ai>
2025-03-09 17:40:59 +0100

    process-initial-data - rename

--- controllers/coverage_controller.py
+++ controllers/coverage_controller.py
@@ -1,6 +1,6 @@
-from helpers import get_insee_with_gps
+from helpers.get_insee_and_post_with_gps import get_insee_and_post_with_gps
 
 class CoverageController:
 
     def get_citycode(self, lon, lat):
-        return get_insee_with_gps(lon, lat)
+        return get_insee_and_post_with_gps(lon, lat)

83c7dfe09fae3fe5e8e3353875b4c4aad0fac6e7
bencebihacsi <bence.bihacsi@smartpoint.ai>
2025-03-09 17:40:44 +0100

    process-initial-data - migration corespondent model

--- models/coverage.py
+++ models/coverage.py
@@ -1,3 +1,5 @@
+from sqlalchemy import UniqueConstraint
+
 from db import db
 
 class NetworkCoverage(db.Model):
@@ -5,8 +7,10 @@ class NetworkCoverage(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     operator = db.Column(db.Integer, nullable=False)
-    insee_code = db.Column(db.String(10), nullable=False)
+    insee_code = db.Column(db.Integer, nullable=False)
+    post_code = db.Column(db.Integer, nullable=False)
     type_2g = db.Column(db.Boolean, nullable=False, default=False)
     type_3g = db.Column(db.Boolean, nullable=False, default=False)
     type_4g = db.Column(db.Boolean, nullable=False, default=False)
 
+    __table_args__ = (UniqueConstraint('operator', 'insee_code', 'post_code','type_2g','type_3g','type_4g', name='unique_coverage_data'),)

d0d70274cd3a5c4b790b07b909f59c405b19aa27
bencebihacsi <bence.bihacsi@smartpoint.ai>
2025-03-09 17:47:14 +0100

    process-initial-data - coverage endpoint

--- controllers/coverage_controller.py
+++ controllers/coverage_controller.py
@@ -1,6 +1,7 @@
 from helpers.get_insee_and_post_with_gps import get_insee_and_post_with_gps
-
+from flask import jsonify
 class CoverageController:
 
     def get_citycode(self, lon, lat):
-        return get_insee_and_post_with_gps(lon, lat)
+        insee, post =  get_insee_and_post_with_gps(lon, lat)
+        return jsonify({"insee": insee, "post": post})
